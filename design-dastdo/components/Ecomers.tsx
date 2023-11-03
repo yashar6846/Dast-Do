da6cddf98ece4ea2159606297606f333c9d01b97
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-11-03 13:52:00 +0300

    new project flochared

---
+++ design-dastdo/components/Ecomers.tsx
@@ -0,0 +1,139 @@
+
+
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+function Ecomers() {
+  return (
+    <section className="text-gray-600 body-font">
+  <div className="container px-4 py-12 mx-auto">
+    <div className="flex flex-wrap -m-4">
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <div className="block relative h-48 rounded overflow-hidden">
+       
+        </div>
+        <div className="mt-4">
+        <Image
+          src="https://m.media-amazon.com/images/I/71Thl6t2fNL._AC_SY310_.jpg"
+          alt="Picture of the author"
+          width={200}
+          height={200}
+        /> 
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p> 
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <div className="block relative h-48 rounded overflow-hidden">
+              </div>
+        <div className="mt-4">
+        <Image
+          src="https://m.media-amazon.com/images/I/71Thl6t2fNL._AC_SY310_.jpg"
+          alt="Picture of the author"
+          width={200}
+          height={200}
+        />
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+          <p className="mt-1">$21.15</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <div className="block relative h-48 rounded overflow-hidden">
+              </div>
+        <div className="mt-4">
+        <Image
+          src="https://m.media-amazon.com/images/I/71Thl6t2fNL._AC_SY310_.jpg"
+          alt="Picture of the author"
+          width={200}
+          height={200}
+        />
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+          <p className="mt-1">$12.00</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <div className="block relative h-48 rounded overflow-hidden">
+              </div>
+        <div className="mt-4">
+        <Image
+          src="https://m.media-amazon.com/images/I/71Thl6t2fNL._AC_SY310_.jpg"
+          alt="Picture of the author"
+          width={200}
+          height={200}
+        />
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+          <p className="mt-1">$18.40</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <div className="block relative h-48 rounded overflow-hidden">
+              </div>
+        <div className="mt-4">
+        <Image
+          src="https://m.media-amazon.com/images/I/71Thl6t2fNL._AC_SY310_.jpg"
+          alt="Picture of the author"
+          width={200}
+          height={200}
+        />
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+          <p className="mt-1">$16.00</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <div className="block relative h-48 rounded overflow-hidden">
+              </div>
+        <div className="mt-4">
+        <Image
+          src="https://m.media-amazon.com/images/I/71Thl6t2fNL._AC_SY310_.jpg"
+          alt="Picture of the author"
+          width={200}
+          height={200}
+        />
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+          <p className="mt-1">$21.15</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <div className="block relative h-48 rounded overflow-hidden">
+              </div>
+        <div className="mt-4">
+        <Image
+          src="https://m.media-amazon.com/images/I/71Thl6t2fNL._AC_SY310_.jpg"
+          alt="Picture of the author"
+          width={200}
+          height={200}
+        />
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+          <p className="mt-1">$12.00</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <div className="block relative h-48 rounded overflow-hidden">
+                </div>
+        <div className="mt-4">
+        <Image
+          src="https://m.media-amazon.com/images/I/71Thl6t2fNL._AC_SY310_.jpg"
+          alt="Picture of the author"
+          width={200}
+          height={200}
+        />
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+          <p className="mt-1">$18.40</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  )
+}
+
+export default Ecomers

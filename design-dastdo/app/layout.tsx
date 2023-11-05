b8f61f13c9f5fbf2b716e001c2911e8c200e60a2
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-11-05 21:36:21 +0300

    plugin desigen

--- design-dastdo/app/layout.tsx
+++ design-dastdo/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import Ecomers from '@/components/Ecomers'
+import Content from '@/components/Content'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +22,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-          <Ecomers />
+          {/* <Ecomers /> */}
+          <Content /> 
         {children}
         <Footer />
         </body>

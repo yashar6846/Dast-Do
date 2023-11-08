a9e974b2dcb2fbce59f96d259a1745fb05305ab0
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-11-08 17:24:38 +0300

    add proctece

--- dast_do/src/components/Navbar/index.js
+++ dast_do/src/components/Navbar/index.js
@@ -1,9 +1,46 @@
 "use client";
 
+import { adminNavOptions, navOptions } from "@/context/utils";
 import { Fragment } from "react";
 
 const isAdminView = false;
 const isAuthUser = false;
+const user = {
+    role: 'admin',
+}
+
+function NavItems({ }) {
+    return (
+      <div
+        className="items-center justify-between w-full md:flex md:w-auto"
+        id="nav-items">
+        <ul
+          className="flex flex-col p-4 md:p-0 mt-4 font-medium  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white"
+        >
+          {isAdminView
+            ? adminNavOptions.map((item) => (
+                <li
+                  className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0"
+                  key={item.id}
+                  onClick={() => router.push(item.path)}
+                >
+                  {item.label}
+                </li>
+              ))
+              :navOptions.map((item) => (
+                <li
+                  className="cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0"
+                  key={item.id}
+                  onClick={() => router.push(item.path)}
+                >
+                  {item.label}
+                </li>
+              ))}
+              </ul>
+              </div>
+    )
+            };
+            
 
 export default function Navbar() {
   return (
@@ -16,11 +53,43 @@ export default function Navbar() {
             </span>
           </div>
           <div className="flex md:order-2 gap-2">
-            <Fragment>
-              <button className="">Account</button>
-              <button className="">Cart</button>
-            </Fragment>
+            {!isAdminView && isAuthUser ? (
+                <Fragment>
+                <button className="">Account</button>
+                <button className="">Cart</button>
+              </Fragment>
+            ): null}
+            {
+                user?.role === 'admin' ?
+                isAdminView ? <button>Client View</button> : <button>Admin View</button>
+                :null }
+            {isAuthUser ? <button>Logout</button> : <button>Login</button>}
+          
+          <button
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-sticky"
+              aria-expanded="false"
+            
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
           </div>
+          <NavItems />
         </div>
       </nav>
     </>
